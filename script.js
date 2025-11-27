6340acb5499655694464c144b6f1e63f02366ec6
QalandarovShokhjahon <shokhjaxonqalandarov@gmail.com>
2025-11-27 18:45:41 +0500

    initial

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Object → bu ma’lumotlarni kalit va qiymat (key–value) ko‘rinishida saqlaydigan tuzilma.
+let shaxs = {
+    ism: 'Shokhjahon',
+    familiya: 'Qalandarov',
+    kasbi: 'Dasturchi',
+    yosh: 18
+}
+console.log(shaxs);
